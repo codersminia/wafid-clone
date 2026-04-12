ba0d57e24bbe3155ca5f5586ad38bcc18f58dd2b
codersminia <codersminia@gmail.com>
2026-04-12 07:46:48 +0500

    job

--- app/Mail/ContactInquiryMail.php
+++ app/Mail/ContactInquiryMail.php
@@ -3,11 +3,12 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Setting;
 
-class ContactInquiryMail extends Mailable
+class ContactInquiryMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
